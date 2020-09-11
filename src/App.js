3b77d5cb7b373ada50915ada8b0d0d4f86dfb85d
giwonjung <2c6829@naver.com>
2020-09-11 23:57:11 +0900

    rd

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/layouts/Navbar';
-import Footer from './components/layouts/Footer';
-// react router import 
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+// react router import
+import { Switch, Route } from "react-router-dom";
 
 // Import pages
-import Home from './components/pages/Home';
-import News from './components/pages/News';
-import Contacts from './components/pages/Contacts';
-import Details from './components/pages/Details';
-import NotFoundPage from './components/pages/NotFoundPage';
-
-
+import Home from "./components/pages/Home";
+import News from "./components/pages/News";
+import Contacts from "./components/pages/Contacts";
+import Details from "./components/pages/Details";
+import NotFoundPage from "./components/pages/NotFoundPage";
 
 // 자동정렬 : 시프트 + 옵션 + F
 
@@ -23,7 +21,7 @@ class App extends Component {
       <div>
         <Navbar />
         <Switch>
-          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/" component={Home} /> */}
           <Route path="/news" component={News} />
           <Route path="/contacts" component={Contacts} />
           <Route path="/details" component={Details} />
