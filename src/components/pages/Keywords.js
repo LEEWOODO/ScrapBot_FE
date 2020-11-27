b695865018eec692f2e73d9bf4a9dd36680d371b
dnejdzlr2 <dnejdzlr2@naver.com>
2020-11-28 01:07:25 +0900

    키워드 조회 완료

--- src/components/pages/Keywords.js
+++ src/components/pages/Keywords.js
@@ -1,16 +1,62 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-import { InfoConsumer } from '../context';
-import KeywordsCard from '../KeywordsCard';
 import { Link } from 'react-router-dom';
+import { InfoConsumer } from '../context';
 
 class Keywords extends Component {
+    id = 0;
     state = {
-        
-        keywords : [
-            "봄","여름","가을","겨울"
-            
+
+        // keywords : [
+        //     "봄","여름","가을","겨울"
+
+        // ],
+
+        userEmail: "dnejdzlr2@naver.com",
+
+        keywords: [
+            // {
+            //     idx : 0,
+            //     title: "title"
+            // }
         ],
-        inputKeyword : "",
+        inputKeyword: "",
+    }
+
+
+    async componentDidMount() {
+       await axios.get('https://dog.ceo/api/breeds/image/random')
+            .then(response => {
+                console.log("rio : " + response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+
+       await axios.get('http://localhost:8080/api/user/info/' + this.state.userEmail)
+            .then(response => {
+                console.log("rio : " + response.data);
+
+                let keywords = response.data.keywords;
+                console.log(keywords);
+                let indexNumber =0;
+                keywords=keywords.map(function(element){return {idx: indexNumber++ ,title:element}});
+
+                this.setState({ keywords });
+
+
+                // this.keywords({
+                //     keywords: response.data.keywords
+                // });
+
+
+
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
     }
 
     // 키워드 변수
@@ -34,23 +80,33 @@ class Keywords extends Component {
     }
 
     // 키워드 추가 
-    handleCreate = (e) => {
+    handleCreate = (data) => {
+        console.log("data :" + data);
         this.setState({
-            keywords: this.state.keywords.concat(this.state.inputKeyword),
-            inputKeyword : ''
+            keywords: this.state.keywords.concat({ id: this.id++, title: this.state.inputKeyword }),
+            inputKeyword: ''
         })
+
+        // information: information.concat({ id: this.id++, ...data })
+
 
     }
 
     // 키워드 삭제 
-    handleDelete = (i) => {
-        alert("delete  i:" + i);
+    handleDelete = (title) => {
+        const { keywords } = this.state;
+
+        alert("delete  title:" + title);
         this.setState({
-            keywords: [
-                ...this.state.keywords.slice(0, i),
-                ...this.state.keywords.slice(i+1, this.state.keywords.length)
-            ]
-        })
+            keywords: keywords.filter(keyword => keyword.title !== title),
+        });
+
+        // this.setState({
+        //     keywords: [
+        //         ...this.state.keywords.slice(0, i),
+        //         ...this.state.keywords.slice(i+1, this.state.keywords.length)
+        //     ]
+        // })
     }
 
     // handleSubmit 이벤트 막기
@@ -61,21 +117,23 @@ class Keywords extends Component {
 
     render() {
         const { keywords } = this.state;
-        const { handleChange, handleKeyPress, handleCreate, handleDelete, handleSubmit} = this;
+        const { handleChange, handleKeyPress, handleCreate, handleDelete, handleSubmit } = this;
         const keywordsList = keywords.map(
             (keyword, index) => (
+                console.log("keyword : "+keyword),
+                console.log("index : "+index),
                 // <KeywordsCard key={index} item={keyword}></KeywordsCard>
                 <div key={index} className="container col-5 col-lg-2 mx-auto mb-2">
                     <div style={{ width: '18rem' }}>
                         <div className="card-body">
                             <Link
-                                onClick={() => 
-                                    handleDelete(index)
+                                onClick={() =>
+                                    handleDelete(keyword.title)
                                 }
                                 className="btn btn-outline-dark"
-                                // to='#'
+                            // to='#'
                             >
-                                {keyword} &nbsp;<i className="fas fa-times"></i>
+                                {keyword.title} &nbsp;<i className="fas fa-times"></i>
                             </Link>
                         </div>
                     </div>
@@ -84,57 +142,62 @@ class Keywords extends Component {
             )
         );
         return (
-                <InfoConsumer>
-                    {value => {
-                        return (
-                           
-                            <div className="container-fluid mt-2 mb-3 p-5" >
-                                <h5 className="display-5 font-weight-bold">키워드 등록</h5>
-                                <p>키워드를 등록해두면 관련된 기사들을 확인 할 수 있어요.</p>
-                                <Link to='#'>혹시 키워드와 관련된 알림이 오지 않나요?</Link>
-                                <form className="form-inline my-2 my-lg-0"  onSubmit={this.handleSubmit}>
-                                    <input className="form-control mr-sm-2" type="search" placeholder="키워드를 입력해주세요." aria-label="Search" name="inputKeyword" value={this.state.inputKeyword} onChange={ handleChange }/>
-                                    <button className="btn btn-outline-primary my-2 my-sm-0" type="button" onClick={ handleCreate }>등록</button>
-                                </form>
-                                <p>키워드 등록({this.state.keywords.length}/30)</p>
+            <InfoConsumer>
+                {value => {
+                    return (
 
-                                {/* <div className="row mt-5">
+                        <div className="container-fluid mt-2 mb-3 p-5" >
+                            <h5 className="display-5 font-weight-bold">키워드 등록</h5>
+                            <p>키워드를 등록해두면 관련된 기사들을 확인 할 수 있어요.</p>
+                            <Link to='#'>혹시 키워드와 관련된 알림이 오지 않나요?</Link>
+                            <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit}>
+                                <input className="form-control mr-sm-2" type="search" placeholder="키워드를 입력해주세요." aria-label="Search" name="inputKeyword" value={this.state.inputKeyword} onChange={handleChange} />
+                                <button className="btn btn-outline-primary my-2 my-sm-0" type="button" onClick={handleCreate}>등록</button>
+                            </form>
+                            <p>키워드 등록({this.state.keywords.length}/30)</p>
+
+                            {/* <div className="row mt-5">
                                     {value.keywords.map(item => {
                                         return <KeywordsCard key={item.id} item={item}></KeywordsCard>;
                                     })}
                                 </div> */}
 
-                                <div className="row mt-5">
-                                    {keywordsList}
-                                </div>
+                            <div className="row mt-5">
+                                {keywordsList}
+                            </div>
 
-                                {/* social icons */}
-                                <div className="mt-5">
-                                    <div className="row justify-content-center">
-                                        <div className="col-2">
-                                            <i className="fab fa-facebook-f"></i>
-                                        </div>
-                                        <div className="col-2">
-                                            <i className="fab fa-twitter"></i>
-                                        </div>
-                                        <div className="col-2">
-                                            <i className="fab fa-google-plus-g"></i>
-                                        </div>
-                                        <div className="col-2">
-                                            <i className="fab fa-reddit"></i>
-                                        </div>
-                                        <div className="col-2">
-                                            <i className="fab fa-whatsapp"></i>
-                                        </div>
-                                        <div className="col-2">
-                                            <i className="fab fa-facebook-messenger"></i>
-                                        </div>
+                            <div>
+                                결과 : {JSON.stringify(this.state.keywords)}
+
+                            </div>
+
+                            {/* social icons */}
+                            <div className="mt-5">
+                                <div className="row justify-content-center">
+                                    <div className="col-2">
+                                        <i className="fab fa-facebook-f"></i>
+                                    </div>
+                                    <div className="col-2">
+                                        <i className="fab fa-twitter"></i>
+                                    </div>
+                                    <div className="col-2">
+                                        <i className="fab fa-google-plus-g"></i>
+                                    </div>
+                                    <div className="col-2">
+                                        <i className="fab fa-reddit"></i>
+                                    </div>
+                                    <div className="col-2">
+                                        <i className="fab fa-whatsapp"></i>
+                                    </div>
+                                    <div className="col-2">
+                                        <i className="fab fa-facebook-messenger"></i>
                                     </div>
                                 </div>
                             </div>
-                        )
-                    }}
-                </InfoConsumer>  
+                        </div>
+                    )
+                }}
+            </InfoConsumer>
 
         )
     }
