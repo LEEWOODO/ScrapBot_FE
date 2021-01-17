dfe536c757b4d109f3aa7d958821a26b03f2a2fb
giwonjung <2c6829@naver.com>
2021-01-17 22:36:54 +0900

    port 변경

--- src/components/apis/restApi.js
+++ src/components/apis/restApi.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 
 const getUserinfoByEmail = async (email, component) => {
-    const url = 'http://34.64.253.11:8080/api/user/info/' + email;
+    const url = 'http://34.64.173.25:8090/api/user/info/' + email;
     console.log(url);
     const data = await axios.get(
-        'http://34.64.253.11:8080/api/user/info/' + email
-        // "http://34.64.253.11:8080/api/user/info/2c6829%40naver.com"
+        'http://34.64.173.25:8090/api/user/info/' + email
+        // "http://34.64.173.25:8090/api/user/info/2c6829%40naver.com"
     );
     console.log(data.data);
     component.setState({ userinfo: data.data });
@@ -16,7 +16,7 @@ const getUserinfoByEmail = async (email, component) => {
 //신문사 정보 가져오는 함수s
 const getNewsCompanies = async () => {
     console.log('excute get newscompanies info');
-    const url = 'http://34.64.253.11:8080/api/company-list';
+    const url = 'http://34.64.173.25:8090/api/company-list';
     let { data } = await axios.get(url);
 
     return data;
@@ -34,7 +34,7 @@ const getNewsData = async (id, component) => {
 
     const todayDateString = todayDate.getFullYear() + '' + (month) + '' + (date);
     console.log(todayDateString);
-    const url = 'http://34.64.253.11:8080/api/user/contents/' + id + '/' + todayDateString;
+    const url = 'http://34.64.173.25:8090/api/user/contents/' + id + '/' + todayDateString;
 
     console.log(url);
     let { data } = await axios.get(url);
@@ -54,7 +54,7 @@ const getNewsDataByDate = async (id, date1, component) => {
 
     const todayDateString = todayDate.getFullYear() + '' + (month) + '' + (date);
     console.log(todayDateString);
-    const url = 'http://34.64.253.11:8080/api/user/contents/' + id + '/' + todayDateString;
+    const url = 'http://34.64.173.25:8090/api/user/contents/' + id + '/' + todayDateString;
 
     console.log(url);
     let { data } = await axios.get(url);
@@ -63,7 +63,7 @@ const getNewsDataByDate = async (id, date1, component) => {
 }
 
 const deleteKeyword = async (id, keyword) => {
-    const url = 'http://34.64.253.11:8080/api/user/keyword/delete?id=' + id + '&keyWord=' + keyword;
+    const url = 'http://34.64.173.25:8090/api/user/keyword/delete?id=' + id + '&keyWord=' + keyword;
 
     const result = await axios.put(url);
 
@@ -71,7 +71,7 @@ const deleteKeyword = async (id, keyword) => {
 }
 
 const addKeyword = async (id, keyword) => {
-    const url = 'http://34.64.253.11:8080/api/user/keyword/add?id=' + id + '&keyWord=' + keyword;
+    const url = 'http://34.64.173.25:8090/api/user/keyword/add?id=' + id + '&keyWord=' + keyword;
     console.log(url);
     const result = await axios.put(url);
 
@@ -79,18 +79,18 @@ const addKeyword = async (id, keyword) => {
 }
 
 const testPost = async () => {
-    const result = await axios.put("http://34.64.253.11:8080/api/user/keyword/add?id=12467&keyWord=안녕하세요");
+    const result = await axios.put("http://34.64.173.25:8090/api/user/keyword/add?id=12467&keyWord=안녕하세요");
 
     return result;
 }
 const addNewdCompany = async (companyid, userid) => {
-    const url = 'http://34.64.253.11:8080/api/user/news-comany/add?companyid=' + companyid + '&userid=' + userid;
+    const url = 'http://34.64.173.25:8090/api/user/news-comany/add?companyid=' + companyid + '&userid=' + userid;
     const result = await axios.put(url);
 
     return result;
 }
 const subNewdCompany = async (companyid, userid) => {
-    const url = 'http://34.64.253.11:8080/api/user/news-comany/sub?companyid=' + companyid + '&userid=' + userid;
+    const url = 'http://34.64.173.25:8090/api/user/news-comany/sub?companyid=' + companyid + '&userid=' + userid;
     const result = await axios.put(url);
 
     return result;
