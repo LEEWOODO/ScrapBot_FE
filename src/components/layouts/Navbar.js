f480a489f48d3ea17d989a72bcae7a806053fb69
giwonjung <2c6829@naver.com>
2021-02-02 23:17:37 +0900

    changing navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -3,6 +3,7 @@ import logo from '../../logo.svg';
 import { Link } from 'react-router-dom';
 import Kakao, { getEmail, getEmails, isLogin } from "../../Kakao";
 import { getNewsData, getUserinfoByEmail, testPost } from "../apis/restApi";
+import "./navbar-collapse.css";
 
 class Navbar extends Component {
     constructor(props) {
@@ -73,44 +74,51 @@ class Navbar extends Component {
             </ul>
         )
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                <Link className="navbar-brand ml-5" to="/">
-                    <img src={logo} alt="logo" style={{ width: '35px' }}></img>
-                </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span>
-                        <i className="fas fa-bars" style={{ color: '#fff' }}></i>
-                    </span>
-                </button>
+            <>
+                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                    <Link className="navbar-brand ml-5" to="/">
+                        <img src={logo} alt="logo" style={{ width: '35px' }}></img>
+                    </Link>
+                    <div>{isLogin ? loginView : notLogin}</div>
+                </nav>
+                <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span>
+                            <i className="fas fa-bars" style={{ color: '#fff' }}></i>
+                        </span>
+                    </button> */}
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav m-auto">
-                        <li className="nav-item active">
-                            <Link className="nav-link text-white text-uppercase ml-5" to="/">Home&nbsp;<i className="fas fa-home"></i><span className="sr-only">(current)</span></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white text-uppercase ml-5" to="/news">News</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white text-uppercase ml-5" to="/keywords">키워드 설정</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white text-uppercase ml-5" to="/news-config">신문사 설정</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white text-uppercase ml-5" to="/contacts">Contact Us</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link text-white text-uppercase ml-5" to="/contacts">My Page&nbsp;<i className="fas fa-user"></i></Link>
-                        </li>
-                    </ul>
-                    {/* <form className="form-inline my-2 my-lg-0">
+                    <div id="navbarSupportedContent">
+                        {/* m-auto 빼면 햄버거 없어짐 ㅅㄱ */}
+                        <ul className="nav text-nowrap flex-nowrap" style={{ overflowX: 'auto' }}>
+                            {/* <li className="nav-item active">
+                                <Link className="nav-link text-white text-uppercase ml-5" to="/">Home&nbsp;<i className="fas fa-home"></i><span className="sr-only">(current)</span></Link>
+                            </li> */}
+                            <li className="nav-item">
+                                <Link className="nav-link text-white text-uppercase ml-5" to="/news">News</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white text-uppercase ml-5" to="/keywords">키워드 설정</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white text-uppercase ml-5" to="/news-config">신문사 설정</Link>
+                            </li>
+                            {/* <li className="nav-item">
+                                <Link className="nav-link text-white text-uppercase ml-5" to="/contacts">Contact Us</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link text-white text-uppercase ml-5" to="/contacts">My Page&nbsp;<i className="fas fa-user"></i></Link>
+                            </li> */}
+                        </ul>
+                        {/* <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
                 </form> */}
-                </div>
-                <div>{isLogin ? loginView : notLogin}</div>;
-            </nav>
+                    </div>
+
+                </nav>
+
+            </>
 
         )
     }
