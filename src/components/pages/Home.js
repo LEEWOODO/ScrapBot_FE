e4f7dd13b5a5be46d9a7149aa29215baef9999bc
giwonjung <2c6829@naver.com>
2021-02-08 00:08:58 +0900

    no message

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import { InfoConsumer } from '../context'
 import Info from '../Info';
+import queryStirng from 'query-string'
+import Kakao from "../../Kakao";
+
 
 class Home extends Component {
+    componentWillMount() {
+        const { search } = this.props.location;
+        const queryObj = queryString.parse(search);
+        const { code } = queryObj;
+        console.log(code);
+        Kakao.Auth.setAccessToken(code);
+        console.log(Kakao.Auth.getAccessToken());
+    }
     render() {
         return (
             <div className="container">
