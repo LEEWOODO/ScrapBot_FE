d80d8d2fa97f86e0c93fc06f017930b06b4cf4ad
giwonjung <2c6829@naver.com>
2021-02-08 01:08:55 +0900

    no message

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,32 +6,32 @@ import Kakao from "../../Kakao";
 
 
 class Home extends Component {
-    getCookie = (name) => {
-        const value = "; " + document.cookie;
-        const parts = value.split("; " + name + "=");
-        if (parts.length === 2) return parts.pop().split(";").shift();
-    }
+    // getCookie = (name) => {
+    //     const value = "; " + document.cookie;
+    //     const parts = value.split("; " + name + "=");
+    //     if (parts.length === 2) return parts.pop().split(";").shift();
+    // }
 
     render() {
-        // const { search } = this.props.location;
-        // const queryObj = queryString.parse(search);
-        // const { code } = queryObj;
-        // console.log(code);
-        // Kakao.Auth.setAccessToken(code);
-        // console.log(Kakao.Auth.getAccessToken());
-        const { getCookie } = this;
-        const token = getCookie('authorize-access-token')
-        if (token) {
-            console.log("token:" + token);
-            Kakao.Auth.setAccessToken(token)
-            Kakao.Auth.getStatusInfo(({ status }) => {
-                if (status === 'connected') {
-                    document.getElementById('token-result').innerText = 'login success. token: ' + Kakao.Auth.getAccessToken()
-                } else {
-                    Kakao.Auth.setAccessToken(null)
-                }
-            })
-        }
+        const { search } = this.props.location;
+        const queryObj = queryString.parse(search);
+        const { code } = queryObj;
+        console.log(code);
+        Kakao.Auth.setAccessToken(code);
+        console.log(Kakao.Auth.getAccessToken());
+        // const { getCookie } = this;
+        // const token = getCookie('authorize-access-token')
+        // if (token) {
+        //     console.log("token:" + token);
+        //     Kakao.Auth.setAccessToken(token)
+        //     Kakao.Auth.getStatusInfo(({ status }) => {
+        //         if (status === 'connected') {
+        //             document.getElementById('token-result').innerText = 'login success. token: ' + Kakao.Auth.getAccessToken()
+        //         } else {
+        //             Kakao.Auth.setAccessToken(null)
+        //         }
+        //     })
+        // }
 
         return (
             <div className="container">
